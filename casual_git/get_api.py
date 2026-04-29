144969bc96feef63a67e4a9cbd25fa5b09ef756b
chro11os <neilbragsg@gmail.com>
2026-04-29 11:27:38 +0800

    Update

--- casual_git/get_api.py
+++ casual_git/get_api.py
@@ -1,1 +1,15 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+def get_github_api():
+    load_dotenv()
+    api_key = os.getenv("GITHUB_API_CHROLLOS")
+
+    url = "https://api.github.com/repos/owner/repo/releases/latest"
+    response = requests.get(url)
+
+    data = response.json()
+    print(data.get("name"))
+
+get_github_api()
---
+++ casual_git/get_user_input.py
@@ -0,0 +1,5 @@
+def get_repo_input():
+    repo = input("Enter Repo URL: ")
+
+def get_repo_version():
+    repo_ver = input("Input Package Version: ")
