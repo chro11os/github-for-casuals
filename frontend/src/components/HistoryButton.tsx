02b7ae7c0c2b59927378097ba063ad49673f83e2
chro11os <neilbragsg@gmail.com>
2026-05-03 10:11:39 +0800

    removed

--- frontend/src/components/HistoryButton.tsx
+++
@@ -1,8 +0,0 @@
-function HistoryButton() {
-    return (
-        <button className="bg-medical-dark text-2xl text-medical-bg border-medical-dark border-4 rounded content-center m-7">
-            Results History
-        </button>
-    )
-}
-export default HistoryButton;
--- frontend/src/components/ScanButton.tsx
+++
@@ -1,8 +0,0 @@
-export function ScanButton() {
-    return (
-        <button className="bg-medical-dark text-2xl text-medical-bg border-medical-dark border-4 rounded content-center m-7">
-            Scan
-        </button>
-    )
-}
-export default ScanButton;
