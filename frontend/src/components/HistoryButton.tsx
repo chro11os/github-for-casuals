b9ad554f1c0e18562c1221843f33226c372402ae
chro11os <neilbragsg@gmail.com>
2026-05-03 10:06:06 +0800

    update

---
+++ frontend/src/components/HistoryButton.tsx
@@ -0,0 +1,8 @@
+function HistoryButton() {
+    return (
+        <button className="bg-medical-dark text-2xl text-medical-bg border-medical-dark border-4 rounded content-center m-7">
+            Results History
+        </button>
+    )
+}
+export default HistoryButton;
